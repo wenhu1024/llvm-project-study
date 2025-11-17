eb12bd1a4b38d1b2e1ecc0a0df5a20d9c0e34f7a
Hu Wentao <wentao.hu@terapines.com>
2025-11-17 10:47:59 +0800

    [One] Override getTargetNodeName function for OneTargetLowering

--- llvm/lib/Target/One/OneISelLowering.cpp
+++ llvm/lib/Target/One/OneISelLowering.cpp
@@ -27,30 +27,38 @@ OneTargetLowering::LowerReturn(SDValue Chain, CallingConv::ID CallConv,
                                bool IsVarArg,
                                const SmallVectorImpl<ISD::OutputArg> &Outs,
                                const SmallVectorImpl<SDValue> &OutVals,
-                               const SDLoc &DL, SelectionDAG &DAG) const 
-{
-    SmallVector<CCValAssign,16> RVLocs;
-    CCState CCInfo(CallConv, IsVarArg,DAG.getMachineFunction(),RVLocs,*DAG.getContext());
-    CCInfo.AnalyzeReturn(Outs, RetCC_One);
+                               const SDLoc &DL, SelectionDAG &DAG) const {
+  SmallVector<CCValAssign, 16> RVLocs;
+  CCState CCInfo(CallConv, IsVarArg, DAG.getMachineFunction(), RVLocs,
+                 *DAG.getContext());
+  CCInfo.AnalyzeReturn(Outs, RetCC_One);
 
-    SDValue Glue;
-    SmallVector<SDValue, 4>RetOps(1,Chain);
+  SDValue Glue;
+  SmallVector<SDValue, 4> RetOps(1, Chain);
 
-    for (unsigned i=0,e=RVLocs.size();i<e;++i){
-        CCValAssign &VA=RVLocs[i];
-        assert(VA.isRegLoc()&&"Can only return in registers!");
+  for (unsigned i = 0, e = RVLocs.size(); i < e; ++i) {
+    CCValAssign &VA = RVLocs[i];
+    assert(VA.isRegLoc() && "Can only return in registers!");
 
-        Chain = DAG.getCopyToReg(Chain, DL, VA.getLocReg(), OutVals[i], Glue);
-        Glue=Chain.getValue(1);
-        RetOps.push_back(DAG.getRegister(VA.getLocReg(), VA.getLocVT()));
-    }
+    Chain = DAG.getCopyToReg(Chain, DL, VA.getLocReg(), OutVals[i], Glue);
+    Glue = Chain.getValue(1);
+    RetOps.push_back(DAG.getRegister(VA.getLocReg(), VA.getLocVT()));
+  }
 
-    RetOps[0]=Chain;
+  RetOps[0] = Chain;
 
-    if (Glue.getNode()){
-        RetOps.push_back(Glue);
-    }
+  if (Glue.getNode()) {
+    RetOps.push_back(Glue);
+  }
 
-    return DAG.getNode(OneISD::RET_GLUE,DL,MVT::Other,RetOps);
-                               
+  return DAG.getNode(OneISD::RET_GLUE, DL, MVT::Other, RetOps);
+}
+
+const char *OneTargetLowering::getTargetNodeName(unsigned Opcode) const {
+  switch (Opcode) {
+  case OneISD::RET_GLUE:
+    return "OneISD::RET_GLUE";
+  default:
+    return nullptr;;
+  }
 }
--- llvm/lib/Target/One/OneISelLowering.h
+++ llvm/lib/Target/One/OneISelLowering.h
@@ -26,6 +26,8 @@ public:
                       const SmallVectorImpl<ISD::OutputArg> &Outs,
                       const SmallVectorImpl<SDValue> &OutVals, const SDLoc &DL,
                       SelectionDAG &DAG) const override;
+
+  const char* getTargetNodeName(unsigned Opcode) const override;
 };
 
 } // namespace llvm
